ae811fb8a49307740e478ce1f07e6af2fb210e22
Martina Novoselac <novoselac.martina@gmail.com>
2024-09-15 16:28:53 +0200

    Add project files.

---
+++ RealTImeDate2/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealTImeDate2.Data
+{
+    public class AppDbContext : DbContext
+    { 
+    //POVEZANO S BAZOM I RADI
+        string _connectionString = "Server=LAPTOP-17F35AKM\\SQLEXPRESS;Database=CompanyDatabase2;Trusted_Connection=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=true";
+
+        
+        public DbSet<Employee> Employee {  get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+        
+            
+    }
+}
